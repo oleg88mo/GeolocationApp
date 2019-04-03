c5e474586b6dad6034bcdb7acc8c1ef827accd78
o.monastyrskyi <oleg88mo@gmail.com>
2019-04-03 14:47:59 +0300

    initial dependencies

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import App from "./pages/App";
+import Splash from "./pages/Splash";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import * as serviceWorker from "./serviceWorker";
+
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Splash} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("geolocation"));
+
+serviceWorker.unregister();
